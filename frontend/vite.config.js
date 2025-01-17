52991f43f04f7fef563aede7272b1ab83ee52b7a
Saveli Matjuhhin <saveli.matjuhhin@tuni.fi>
2025-01-17 16:42:42 +0200

    changed backend url

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:3003",
+      '/api': {
+        target: 'https://fso1120-backend.onrender.com',
         changeOrigin: true,
       },
     },
